6d242085508bf3e0b84ceae9086f33d276ed479d
James Richardson <jamesernet@gmail.com>
2026-03-05 15:32:21 -0800

    Refactor of theme guide in prep for upcoming design and brand guidelines...merged the oldlogic and there will be a future refactor here but should be easy to apply the general theme when we have fonts, colors, icons etc etc

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,9 +1,11 @@
+// app/providers.tsx
+
 "use client";
 
 import * as React from "react";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "@/lib/theme";
+import { theme } from "@/theme/theme";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
